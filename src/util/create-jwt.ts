06226b09a5cd3a1ace85d5753258fe01925843f8
unknown <jens.osterberg@gmail.com>
2026-02-15 16:07:44 +0200

    -

--- src/util/create-jwt.ts
+++
@@ -1,7 +0,0 @@
-import jwt from 'jsonwebtoken';
-import { loadEnvVariable } from './load-env-variable';
-
-export function createJWT(payload: Record<string, unknown>, options?: jwt.SignOptions) {
-  const secret = loadEnvVariable('TOKEN_SECRET');
-  return jwt.sign(payload, secret, options);
-}
--- src/util/verify-jwt.ts
+++
@@ -1,7 +0,0 @@
-import { loadEnvVariable } from './load-env-variable';
-import jwt from 'jsonwebtoken';
-
-export function verifyJWT(token: string) {
-  const secret = loadEnvVariable('TOKEN_SECRET');
-  return jwt.verify(token, secret);
-}
