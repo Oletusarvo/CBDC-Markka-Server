60c515e74d293fbbccf8229d876ef95810cc4b5a
unknown <jens.osterberg@gmail.com>
2026-02-17 19:50:50 +0200

    Added functionality for giving new accounts funds

--- src/features/auth/handlers/register-user-handler.ts
+++ src/features/auth/handlers/register-user-handler.ts
@@ -5,6 +5,7 @@ import { userSchema } from '../schemas/register-user-schema';
 import { createHandler } from '../../../utils/create-handler';
 import { db } from '../../../db-config';
 import { hashPassword } from '../../../utils/password';
+import { containsExactly, mint, pick } from '../../currencies/util/currency-util';
 
 export const registerUserHandler = createHandler(
   async (req: ExpressRequest<z.infer<typeof userSchema>>, res) => {
@@ -17,9 +18,54 @@ export const registerUserHandler = createHandler(
         })
         .returning('id');
 
-      await trx('account').insert({
-        user_id: user.id,
-      });
+      const [acc] = await trx('account')
+        .insert({
+          user_id: user.id,
+        })
+        .returning('id');
+
+      const reserveTokens = await trx('currency_object')
+        .whereNull('account_id')
+        .whereIn(
+          'currency_denom_type_id',
+          trx.select('id').from('currency_denom_type').where('value_in_cents', '<=', 2000),
+        )
+        .forUpdate()
+        .skipLocked()
+        .limit(200);
+
+      if (reserveTokens.length > 0 && containsExactly(reserveTokens, 2000)) {
+        //Give the unassigned tokens to the new user.
+        const tokens = pick(reserveTokens, 2000);
+        await trx('currency_object')
+          .whereIn(
+            'id',
+            tokens.map(t => t.id),
+          )
+          .update(
+            tokens.map(t => {
+              return {
+                ...t,
+                account_id: acc.id,
+              };
+            }),
+          );
+      } else {
+        //Mint new tokens to give to the user
+        const mintedTokens = mint(2000);
+        await Promise.all(
+          mintedTokens.map(async t => {
+            await trx('currency_object').insert({
+              account_id: acc.id,
+              currency_denom_type_id: trx
+                .select('id')
+                .from('currency_denom_type')
+                .where({ value_in_cents: t })
+                .limit(1),
+            });
+          }),
+        );
+      }
     });
     return res.status(200).end();
   },
